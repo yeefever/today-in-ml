18e995010d7e84889244c223dd8fe8a0aedea65c
yeefever <76747388+yeefever@users.noreply.github.com>
2023-10-02 00:26:29 -0400

    hello

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   const [date, setDate] = useState(7);
 
   return (<main className="flex min-h-screen flex-col items-center justify-center p-12">
-    <h1 className="text-5xl font-bold mb-2 text-center">Recently in Machine Learning</h1>
+    <h1 className="text-5xl font-bold mb-2 text-center">Recently in Research</h1>
     <h2 className="text-2xl mb-4 text-center">I'm curious about ...</h2>
 
     <div className="flex space-x-4">
